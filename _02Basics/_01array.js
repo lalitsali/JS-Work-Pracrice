3fdcdf65d2188fbe9739d8c8069979c3679b930c
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-20 12:41:25 +0000

    arrays2

---
+++ _02Basics/_01array.js
@@ -0,0 +1,19 @@
+//array
+let arr=[1,2,3,4,6]
+console.log(arr);//[ 1, 2, 3, 4, 6 ]
+arr.push(9)
+console.log(arr);//[ 1, 2, 3, 4, 6, 9 ]
+arr.pop()// remove last element
+console.log(arr);//[ 1, 2, 3, 4, 6 ]
+ let arr2=[8,7,9,9]
+//  arr.push(arr2)
+//  console.log("new array",arr); //[ 1, 2, 3, 4, 6, [ 8, 7, 9, 9 ] ]
+
+ let new_array =arr.concat(arr2)
+
+ console.log("new 2 array",new_array); //[ 1, 2, 3, 4, 6, 8, 7, 9, 9 ]
+let a=10;
+let b=20;
+let c=30;
+let d=40;
+console.log(Array.of(a,b,c,d));//[ 10, 20, 30, 40 ]
