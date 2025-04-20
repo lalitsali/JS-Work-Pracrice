46172699544c422fb335c7abd4b8e700bd5b0f84
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-20 11:25:57 +0000

    concat types

---
+++ _01Basics/_03conversion.js
@@ -0,0 +1,10 @@
+let a=10
+let b="lal"+String(a)
+console.log(b)
+console.log(typeof(b))
+
+
+console.log(1+"2");  //12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+console.log(1+"2");//12
