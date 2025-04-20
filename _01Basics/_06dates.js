7552e4d10a3a5944a7aaa23a62a42ebf950058e3
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-20 12:19:31 +0000

    dates

---
+++ _01Basics/_06dates.js
@@ -0,0 +1,4 @@
+let date=new Date();
+console.log(date);//2025-04-20T12:15:24.129Z
+console.log(date.getDate());//give current date --> 20
+console.log(date.toString());//Sun Apr 20 2025 12:17:36 GMT+0000 (Coordinated Universal Time)
