b47ffbeeafe3e12be0de54fcccefd821966febd4
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-20 11:01:30 +0000

    data Types

---
+++ _01Basics/_02Variable.js
@@ -0,0 +1,7 @@
+
+let name="swami";
+const id=123;
+ var city="Mumbai";
+age=23;
+console.table([name,id,city,age])
+/* var npot use---> blaock scope amd function scope*/
---
+++ _01Basics/_02datatypes.js
@@ -0,0 +1,10 @@
+// alert(3+3)//not work in node.js
+
+// "use strict"; //treat all js code as new version
+
+
+console.log("hello buddy")
+let a="KHUSHI"
+console.log(typeof(a))
+console.log(typeof undefined)//undefined
+console.log(typeof null);//object
