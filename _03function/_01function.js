0cc6858acdab15369f806d6ee16fc3f6ca87600b
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-23 10:05:33 +0000

    part 1 function basic

---
+++ _03function/_01function.js
@@ -0,0 +1,29 @@
+
+
+
+function sayMony(){
+    console.log("h");
+    console.log("i");
+}
+
+//sayMony()//h
+         //i
+
+
+function add(a,b){
+    return a+b;
+}  
+console.log(add(2,3))//5
+console.log(add(2,7,7))//
+
+// check value is getting or not
+
+function informa(name){
+   if(name===undefined){
+         console.log("pass the value");
+   }
+   return `${name} just logged in`
+}
+
+// console.log(informa("sachin"))//sachin just logged in
+console.log(informa())//pass the value
