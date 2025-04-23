d6e0085ec84994600d8d368e95091c2007e4b62e
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-23 09:46:59 +0000

    end of full concept of object

---
+++ _02Basics/_04object.js
@@ -0,0 +1,17 @@
+
+// destructuring objects
+const user = {   
+    name: "swami",
+    surname: "patil",
+    age: 25,
+    location: "Jalgaon",
+    email: "@google.com",
+    isloggedin: false,
+    lstlogginDays: ["Monday", "Saturday"]       
+}
+
+
+// value extraction
+const {lstlogginDays :insbalhf} = user
+// console.log(lstlogginDays)//[ 'Monday', 'Saturday' ]    
+console.log(insbalhf)//[ 'Monday', 'Saturday' ]    
