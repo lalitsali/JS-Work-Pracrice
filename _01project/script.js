252a056644893089bdf8c949dfd48a2a3e190c8c
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-30 10:29:55 +0000

    project 1

---
+++ _01project/script.js
@@ -0,0 +1,40 @@
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector("body")
+
+const text=document.querySelector("h2")
+
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+     console.log(e);
+     console.log(e.target);
+    if(e.target.id=='grey'){
+    //   body.style.backgroundColor='grey'
+        body.style.backgroundColor=e.target.id  
+        text.style.color='pink'
+     }
+     if(e.target.id=='white'){
+        //   body.style.backgroundColor='white'
+              body.style.backgroundColor=e.target.id  
+              text.style.color='red'
+      }
+        
+    if(e.target.id=='yellow'){
+      //   body.style.backgroundColor='yellow'
+        body.style.backgroundColor=e.target.id  
+        text.style.color='green'
+        }    
+    if(e.target.id=='blue'){
+        //   body.style.backgroundColor='blue'
+            body.style.backgroundColor=e.target.id  
+    text.style.color='orange'
+        }
+
+
+
+
+    });
+
+
+});
