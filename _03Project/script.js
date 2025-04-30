99085139c75566a2d2d817051328e6d227b26767
lalitsali <159426842+lalitsali@users.noreply.github.com>
2025-04-30 11:33:59 +0000

    Digital Clock Project 3

--- _03Project/script.js
+++ _03Project/script.js
@@ -7,6 +7,6 @@ setInterval(function(){
     let date=new Date();
     // console.log(date.toLocaleTimeString());
 
-    clock.innerHTML=date.toLocaleTimeString();
+    clock.innerHTML=date.toLocaleTimeString();   
 
 },1000)
